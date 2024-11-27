d1e2b16f50b9306545b50be6a5584329424fc1da
Sysstuk <64896289+Sysstuk@users.noreply.github.com>
2024-11-27 15:43:39 -0500

    Add files via upload

---
+++ InTune Remediations/Bloatware Removal/Bloatware Removal.ps1
@@ -0,0 +1,10 @@
+try {
+    $packages = @("*3d*","*maps*","*mixedreality*","*phone*","*skype*","*solitaire*","*windowscommunicationsapps*","Microsoft.XboxIdentityProvider","Microsoft.BingTravel","Microsoft.BingHealthAndFitness","Microsoft.BingFoodAndDrink","Microsoft.XboxApp","Microsoft.BingSports","Microsoft.WindowsPhone","*zune*","*OutlookForWindows*","Microsoft.XboxGamingOverlay","Microsoft.XboxGameOverlay","Microsoft.XboxSpeechToTextOverlay","Microsoft.XboxTCUI")
+    foreach ($package in $packages) {
+        Get-AppxPackage $package | Remove-AppxPackage -ErrorAction stop 
+        Write-Host "$package apps successfully removed"
+    }
+}
+catch { 
+    Write-Error "Error removing Bloatware" 
+}
